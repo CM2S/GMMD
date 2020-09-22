42a6e877dd2244e5ef6196387864a416bc74816f
josevilacha <zulu38luis@gmail.com>
2020-09-22 16:57:26 +0100

    Changes in print final message

--- geommicgen/iofuncs/printing.py
+++ geommicgen/iofuncs/printing.py
@@ -25,19 +25,21 @@ def print_femsh_output(filepath):
     print_to_file("\t Output file: {0}".format(filepath))
 
 
-def print_final_message(time, total_overlap, number_iterations, sample, max_overlap):
-    printToFile("=" * 80)
-    printToFile("**RESULTS**\n")
-    printToFile("Total iterations: {0}".format(number_iterations))
-    printToFile("Simulation time: {:.3f} s".format(time))
-    printToFile("Total overlap: {:.2e}".format(total_overlap))
-    printToFile("Maximum overlap: {:.2e}".format(max_overlap))
 def print_rgmsh_output(filepath):
     print_to_file("Regular grid mesh:")
     print_to_file("\t Output file: {0}".format(filepath))
 
 
 def printToFile(message, end="\n"):
+def print_final_message(time, total_overlap, number_iterations, max_overlap):
+    print_to_file("")
+    print_to_file("MD simulation results")
+    print_to_file("=" * 80 + "\n")
+    print_to_file("Total iterations: {0}".format(number_iterations))
+    print_to_file("Simulation time: {:.3f} s".format(time))
+    print_to_file("Total overlap: {:.2e}".format(total_overlap))
+    print_to_file("Maximum overlap: {:.2e}".format(max_overlap))
+    print_to_file("\n")
     """Print to the screen file of corresponding to the current `.Particle` object"""
     with open("temp.screen", "a") as screen:
         print(message, file=screen, end=end)
