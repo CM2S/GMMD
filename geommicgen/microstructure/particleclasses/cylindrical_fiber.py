6ceb72e633f1a7adfb4386ae3a2a4aa76fd5d42e
josevilacha <zulu38luis@gmail.com>
2021-03-08 17:34:25 +0000

    Cleaned up cylindrical_fiber.py

--- geommicgen/microstructure/particleclasses/cylindrical_fiber.py
+++ geommicgen/microstructure/particleclasses/cylindrical_fiber.py
@@ -1,20 +1,7 @@
-"""
-Module containing all the Particle abstract class and all its subclasses.
-
-Each subclass of the Particle class is a type of particle. This module includes the Ellipse,
-Disk, CylindricalFiber, Ellipsoid and Shpere classes.
-"""
+"""Module containing the cylindrical fiber particle class."""
 from __future__ import annotations
 
-import abc
-import time
-
-
-from itertools import cycle
-
 import numpy as np
-
-from scipy import integrate
 
 from microstructure.particleclasses import Disk, Particle
 
