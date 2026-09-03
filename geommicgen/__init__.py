fa4bdef46979d07e7a1c2f63f29cab917f5feb48
Rui Coelho <ruicoelhopedro@gmail.com>
2026-09-03 16:06:40 +0100

    Upgrade to `pyproject.toml`

--- geommicgen/__init__.py
+++ geommicgen/__init__.py
@@ -1,5 +1,11 @@
 from geommicgen import app
 
+__title__ = "geommicgen"
+__author__ = "CM2S"
+__copyright__ = "2026, CM2S"
+__license__ = "BSD-3-Clause"
+__version__ = "0.1.0"
+
 
 def run():
     """Entry point for the execution command."""
